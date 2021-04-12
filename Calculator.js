684990eeea1806312b99ac3c646d9a66744e63e3
Aman Dalal <amandalal017@gmail.com>
2021-04-12 15:08:38 +0530

    modified display

--- Calculator.js
+++ Calculator.js
@@ -1,33 +1,33 @@
 //Function to get button values in display
 function call(elem){
     var val = elem.value;
-    if(document.getElementById("display").innerHTML == 0){
-        document.getElementById("display").innerHTML = val;
+    if(document.getElementById("display").value == 0){
+        document.getElementById("display").value = val;
     }else{
-        document.getElementById("display").innerHTML += val;
+        document.getElementById("display").value += val;
     }
     
 }
  
 //Function to clean all the content of display
  function clean(){
-     document.getElementById("display").innerHTML = 0;
+     document.getElementById("display").value = 0;
     
  }
 
 //Function to clear last digit in display
 function cancel(){
-     var content = document.getElementById("display").textContent;
-     document.getElementById("display").innerHTML = content.substring(0, content.length-1);
+     var content = document.getElementById("display").value;
+     document.getElementById("display").value = content.substring(0, content.length-1);
      
 }
 
 //Function to calculate input content
 function cal(){
-    var content = document.getElementById("display").innerHTML;
+    var content = document.getElementById("display").value;
     
     if(content){
-      document.getElementById("display").innerHTML = eval(content) ; 
+      document.getElementById("display").value = eval(content) ; 
     }
 
 }
